c3f9e6c454069dd300795473e1d3c25c49189f20
mahid <mo47da@gmail.com>
2020-07-19 00:57:35 +0600

    clutter deleted

--- server.js
+++ server.js
@@ -26,39 +26,7 @@ app.use("/api", require("./routes/registration"));
 app.use("/api", require("./routes/login"));
 
 
-/*
-const users = [];
-app.get("/users", (req, res) => {
-  res.json(users);
-});
-app.post("/users", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = { name: req.body.name, password: hashedPassword };
-    users.push(user);
-    res.status(201).send();
-  } catch {
-    res.status(500).send();
-  }
-});
-*/
 
-app.post("/users/login", async (req, res) => {
-  try {
-    const user = await user.findOne({ username: req.body.username });
-    if (user == null) {
-      return res.status(400).send("Cannot find user");
-    } else {
-      if (await bcrypt.compare(req.body.password, user.password)) {
-        res.send("Success");
-      } else {
-        res.send("Not Allowed");
-      }
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => console.log("Server started on port " + PORT));
